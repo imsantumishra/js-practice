19060f4c94fc51c9fd46b0bee36b6f1ed2c1bcd0
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-23 18:49:32 +0000

    datatype

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 +3) // we are using nodejs, not browser
+
+console.log(3+3);
+
+let name = "santu"
+let age = 21
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint => use like trading big website 
+//string => ""/''
+//boolean => true/false
+//null => standalone value
+//undefind 
+//symbol => unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
