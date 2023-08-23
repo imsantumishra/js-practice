9e8a5314be8e3d16fa4ddba95026b957b259672c
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-23 18:21:19 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "santu@gmail.com"
+var accountPassword ="2238"
+accountCity ="Kolkata"
+
+accountEmail = "hd@gmail.com"
+accountPassword = "58633412"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
