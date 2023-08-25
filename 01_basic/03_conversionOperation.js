d31ea99ea2fffde366cf604becb33371e4d15b8a
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-25 18:28:09 +0000

    conversion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+/**let score = false
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+**/
+
+// Note
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+/**let isLoggedIn = "Xavian"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+**/
+// Note
+//1 => true ; 0 => false
+// "" => false
+// "Santu" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
