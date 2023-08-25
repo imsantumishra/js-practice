9774a527fecc4c8233fc281d78725b7b4f2c35c9
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-25 19:19:32 +0000

    conversion of data type

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,8 +23,35 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "Santu" => true
 
-let someNumber = 33
+/**let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof (stringNumber));
+**/
+
+
+//********************************Operations****************************************
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2**5);
+// console.log(2%6);
+
+/**let str1 = "Mishra"
+let str2 = "Santu "
+let str3 = str2 + str1;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");**/
+
+console.log(true+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
