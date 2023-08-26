3ff688923054841355200163b6cf2e2eb0cb2349
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-26 19:03:57 +0000

    comparision data type

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1); 
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// this comparision can create problem
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+//
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
