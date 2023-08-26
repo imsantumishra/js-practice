e0a3ddda3daa62f9e3083f6aa2f10196bf9a8071
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-26 19:27:32 +0000

    datatype summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 type : String , Number, Boolean,  null, undefined, symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('343')
+const anotherId = Symbol('343')
+
+console.log(id === anotherId);
+
+const bigNumber = 753363363352211223n
+
+console.log(bigNumber)
+
+const heros = ["saktiman", "naagraj", "doga"]
+let myObj ={
+    name: "santu",
+    age: 22
+}
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+myFunction();
+console.log(typeof anotherId);
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
