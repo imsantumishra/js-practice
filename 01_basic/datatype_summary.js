9db345e247b4437648d4c591012218c9c31d7cd1
Santu Mishra <141560317+imsantumishra@users.noreply.github.com>
2023-08-27 18:31:00 +0000

    datatype summary

--- 01_basic/datatype_summary.js
+++ 01_basic/datatype_summary.js
@@ -2,7 +2,7 @@
 
 // 7 type : String , Number, Boolean,  null, undefined, symbol, BigInt
 
-
+/** 
 const score = 100
 const scoreValue = 100.4
 
@@ -30,9 +30,33 @@ const myFunction = function(){
     console.log("Hello World");
 }
 myFunction();
-console.log(typeof anotherId);
+console.log(typeof anotherId);*/
 
 // Reference (Non primitive)
 
 //Array, Objects, Functions
 
+//https://262.ecma-international.org/5.1/#sec-11.4.3/
+
+
+// ***********************************************************************************
+// Stack (Primitive), Heap (Non- Primitive)
+
+let myYoutubeName = "SantuMishra"
+
+let anotheName = myYoutubeName
+anotheName = "THIs HEllo"
+console.log(myYoutubeName);
+console.log(anotheName);
+
+
+let userOne = {
+    email: "hello@google.com",
+    upi: "heloo@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "hisantu@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
